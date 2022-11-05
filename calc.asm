20261c35db2dda4d02a872b86eb53dfd03e4591f
Ygrik2003 <59407669+Ygrik2003@users.noreply.github.com>
2022-11-05 13:36:28 +0300

    Update calc.asm

--- calc.asm
+++ calc.asm
@@ -65,61 +65,61 @@ go_end:
 HexChar2Dec ENDP
 
 Hex2Dec PROC ;В esi хранится ссылка на строку
-	LOCAL outputInt 	:DWORD
-	LOCAL digitMultiplier 	:DWORD
+  LOCAL outputInt 		:DWORD
+  LOCAL digitMultiplier 	:DWORD
 
-	mov eax, maxReadHexLen
-	cmp countChars, eax
-	jna start_calc
+  mov eax, maxReadHexLen
+  cmp countChars, eax
+  jna start_calc
 
-	mov countChars, eax
+  mov countChars, eax
 
 start_calc:
-	mov outputInt, 0
-	mov digitMultiplier, 1
-	add esi, countChars
-	dec esi
+  mov outputInt, 0
+  mov digitMultiplier, 1
+  add esi, countChars
+  dec esi
 
 for_char:
-	xor eax, eax
-	mov al, [esi]
-	call HexChar2Dec
+  xor eax, eax
+  mov al, [esi]
+  call HexChar2Dec
 
-	mul digitMultiplier
+  mul digitMultiplier
 	
   add outputInt, eax
   shl digitMultiplier, 4d
 
   dec esi
   dec countChars
-  ;cmp countChars, -1
-	jnz for_char
+  
+  jnz for_char
 
-	mov eax, outputInt
-	ret
+  mov eax, outputInt
+  ret
 Hex2Dec ENDP ;Результат храниться в eax
 
 Dec2Hex PROC
-	xor ebx, ebx
-	xor edx, edx
+  xor ebx, ebx
+  xor edx, edx
 
-	mov esi, maxHexLen
-	dec esi
+  mov esi, maxHexLen
+  dec esi
 
-	mov cx, WORD PTR [maxHexLen]
+  mov cx, WORD PTR [maxHexLen]
 for_num:
-	mov bx, ax
-	and bx, 1111b
-	mov edx, offset sHex
-	mov dl, BYTE PTR [edx + ebx]
-	
-	mov [hexResultNumber + esi], BYTE PTR dl
-	dec esi
-	shr eax, 4d
+  mov bx, ax
+  and bx, 1111b
+  mov edx, offset sHex
+  mov dl, BYTE PTR [edx + ebx]
+  
+  mov [hexResultNumber + esi], BYTE PTR dl
+  dec esi
+  shr eax, 4d
 
-	dec cx
-	jnz for_num
-	ret
+  dec cx
+  jnz for_num
+  ret
 Dec2Hex ENDP
 
 Main PROC
@@ -136,51 +136,51 @@ Main PROC
   invoke ReadConsole, hStdin, ADDR buffer, 60, ADDR countChars, 0
   sub countChars, 2
 	
-	mov esi, offset buffer
-	call Hex2Dec
-	mov decFirstNumber, eax
+  mov esi, offset buffer
+  call Hex2Dec
+  mov decFirstNumber, eax
 
-	invoke WriteConsoleA, hStdout, ADDR sEnterSecondNumber, sEnterSecondLen, 0, 0
-	invoke ReadConsole, hStdin, ADDR buffer, 60, ADDR countChars, 0
-	sub countChars, 2
+  invoke WriteConsoleA, hStdout, ADDR sEnterSecondNumber, sEnterSecondLen, 0, 0
+  invoke ReadConsole, hStdin, ADDR buffer, 60, ADDR countChars, 0
+  sub countChars, 2
 
-	mov esi, offset buffer
-	call Hex2Dec
-	mov decSecondNumber, eax
+  mov esi, offset buffer
+  call Hex2Dec
+  mov decSecondNumber, eax
 
-	invoke crt_printf, ADDR check, decFirstNumber, decSecondNumber
-	
-	mov eax, decFirstNumber
-	add eax, decSecondNumber
+  invoke crt_printf, ADDR check, decFirstNumber, decSecondNumber
+  
+  mov eax, decFirstNumber
+  add eax, decSecondNumber
 
-	call Dec2Hex
+  call Dec2Hex
 
-	invoke WriteConsoleA, hStdout, ADDR sAdd, sOperationLen, 0, 0
-	invoke WriteConsoleA, hStdout, ADDR hexResultNumber, maxHexLen, 0, 0
+  invoke WriteConsoleA, hStdout, ADDR sAdd, sOperationLen, 0, 0
+  invoke WriteConsoleA, hStdout, ADDR hexResultNumber, maxHexLen, 0, 0
 
-	mov eax, decFirstNumber
-	sub eax, decSecondNumber
+  mov eax, decFirstNumber
+  sub eax, decSecondNumber
 
-	call Dec2Hex
+  call Dec2Hex
 
-	invoke WriteConsoleA, hStdout, ADDR sSub, sOperationLen, 0, 0
-	invoke WriteConsoleA, hStdout, ADDR hexResultNumber, maxHexLen, 0, 0
+  invoke WriteConsoleA, hStdout, ADDR sSub, sOperationLen, 0, 0
+  invoke WriteConsoleA, hStdout, ADDR hexResultNumber, maxHexLen, 0, 0
 
-	mov eax, decFirstNumber
-	mul decSecondNumber
+  mov eax, decFirstNumber
+  mul decSecondNumber
 
-	call Dec2Hex
+  call Dec2Hex
 
-	invoke WriteConsoleA, hStdout, ADDR sMul, sOperationLen, 0, 0
-	invoke WriteConsoleA, hStdout, ADDR hexResultNumber, maxHexLen, 0, 0
+  invoke WriteConsoleA, hStdout, ADDR sMul, sOperationLen, 0, 0
+  invoke WriteConsoleA, hStdout, ADDR hexResultNumber, maxHexLen, 0, 0
 
-	mov eax, decFirstNumber
-	div decSecondNumber
+  mov eax, decFirstNumber
+  div decSecondNumber
 
-	call Dec2Hex
+  call Dec2Hex
 
-	invoke WriteConsoleA, hStdout, ADDR sDiv, sOperationLen, 0, 0
-	invoke WriteConsoleA, hStdout, ADDR hexResultNumber, maxHexLen, 0, 0
+  invoke WriteConsoleA, hStdout, ADDR sDiv, sOperationLen, 0, 0
+  invoke WriteConsoleA, hStdout, ADDR hexResultNumber, maxHexLen, 0, 0
 
   invoke ExitProcess, 0
 Main ENDP
